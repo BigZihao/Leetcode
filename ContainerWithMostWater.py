4709fa89361b15bc70bab1049a632be650552441
Zihao Zhang <zihao.zhang.ustb@gmail.com>
2017-01-13 17:52:23 -0500

    Update ContainerWithMostWater.py

--- ContainerWithMostWater.py
+++ ContainerWithMostWater.py
@@ -1,5 +1,9 @@
 # Well, the key is to prove you will get the max every time you move the small node 
 
+# the area is determined by the small one. say height=[1,2,3,4]. when you got the first area abs(4-1)*min(1,4)=3, this is determined by the left node 1
+# you won't have anything bigger than 3 if you fix left node
+# so this trick save you from looping another round, form O(n2) to O(n)
+
 class Solution(object):
     def maxArea(self, height):
         """

8951934353e266c2a5c59d5650d96eca77d316d1
BigZihao <zihao.zhang.ustb@gmail.com>
2017-11-07 17:44:55 -0500

    1107

--- Rotate_Function.py
+++ Rotate_Function.py
@@ -15,3 +15,16 @@ def maxRotateFunction(self, A):
 		F+= s - n*last_item
 		max_F = max(max_F, F)
 	return max_F
+
+def maxRotateFunction(self, A):
+	if len(A) == 0:
+		return 0
+	totalSum = sum(A)
+	lMax = 0
+	for i in range(len(A)):
+		lMax+=i*A[i]
+	gMax = lMax
+	for i in range(len(A)-1, 0, -1):
+		lMax += (totalSum - A[i]*len(A))
+		gMax = max(gMax, lMax)
+	return gMax

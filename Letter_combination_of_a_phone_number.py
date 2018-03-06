9268b28b8a5816764452a25051a34a28cf434831
BigZihao <zihao.zhang.ustb@gmail.com>
2018-03-06 13:27:38 -0500

    '0306'

---
+++ Letter_combination_of_a_phone_number.py
@@ -0,0 +1,28 @@
+class Solutions(object):
+	def LetterCombinations(self, digits):
+
+		## time complexity O(4^n)
+		def dfs(num, string, res):
+			if num == length:
+				res.append(string)
+				return
+			for letter in dict[digits[num]]:
+				dfs(num+1, string+letter, res)
+
+		### string is the current path along the graph using DFS, once you hit the num, then store the string to res
+
+		dict = {'2':['a','b','c'],
+                '3':['d','e','f'],
+                '4':['g','h','i'],
+                '5':['j','k','l'],
+                '6':['m','n','o'],
+                '7':['p','q','r','s'],
+                '8':['t','u','v'],
+                '9':['w','x','y','z']
+                }
+        res = []
+        length = len(digits)
+        if length == 0:
+        	return res
+        dfs(0, '', res)
+        return res

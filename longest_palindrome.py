6a84b416aa7771ce48eed24110aaed0785a7ca97
BigZihao <zihao.zhang.ustb@gmail.com>
2018-03-09 18:07:53 -0500

    '0309'

--- longest_palindrome.py
+++ longest_palindrome.py
@@ -1,17 +1,21 @@
-class Solution(object):
+class Solution:
     def longestPalindrome(self, s):
-        ## brute force way is O(n^3)
-
-    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
         n = len(s)
+        if n<=1:
+            return s
         dp = [[0]*n for i in range(n)]
-        count = 0
+        res= ''
         maxlength=1
-        
         for end in range(n):
             dp[end][end] = 1
-            count+=1
-            for start in range(end):
-                if s[start] == s[end] and (start+1>=end-1 or dp[start+1][end-1]) and end-start>maxlength:
-                    res = s[start:end]
+            for start in range(end+1):
+                if s[start] == s[end] and (end-start<=2 or dp[start+1][end-1]):
+                    dp[start][end] = 1
+                    if end-start+1>=maxlength:
+                        res = s[start:end+1]
+                        maxlength = end-start+1
         return res

b1b3066874e00ac7d2d491b73199ad73c39e8d41
BigZihao <zihao.zhang.ustb@gmail.com>
2018-05-21 17:53:10 -0400

    '0521'

--- add_binary.py
+++ add_binary.py
@@ -1,6 +1,38 @@
 #!/usr/bin/python
 ## binary operations
 
+class Solution():
+    def addBinary(self, a, b):
+        if len(a) == 0: return b
+        if len(b) == 0: return a
+        if a[-1] == '1' and b[-1] == '1':
+            return self.addBinary(self.addBinary(a[0:-1], b[0:-1]), '1') + '0'
+        if a[-1] == '0' and b[-1] == '0':
+            return self.addBinary(a[0:-1], b[0:-1]) + '0'
+        else:
+            return self.addBinary(a[0:-1], b[0:-1]) + '1'
+
+    def addBinary(self, a, b):
+        if a == "":
+            return b
+        if b == "":
+            return a
+        a_2 = int(a,2)
+        b_2 = int(b,2)
+        carry = 1
+        while carry != 0:
+            carry = (a_2 & b_2)<<1
+            a_2 = a_2 ^ b_2
+            b_2 = carry
+        return bin(a_2)[2:]
+
+
+
+
+
+
+
+
 a = 60            # 60 = 0011 1100 
 b = 13            # 13 = 0000 1101 
 c = 0
---
+++ spiral_matrix.py
@@ -0,0 +1,18 @@
+class Solution():
+	def spiralOrder(self, matrix):
+		if not matrix or not matrix[0]:
+			return []
+		ans = []
+		n, m = len(matrix), len(matrix[0])
+		u, d, l, r = 0, n-1, 0, m-1
+		while u<d and l<r:
+			ans+=[matrix[u][j] for j in range(l, r)]
+			ans+=[matirx[i][r] for r in range(u, d)]
+			ans+=[matrix[d][j] for j in range(r, l, -1)]
+			ans+=[matrix[i][l] for i in range(d, u, -1)]
+			u, d, l, r = u+1, d-1, l+1, r-1
+		if l == r:
+			ans+=[matrix[i][r] for i in range(u, d+1)]
+		elif u == d:
+			ans+=[matrix[u][j] for j in range(l, r+1)]
+		return ans
